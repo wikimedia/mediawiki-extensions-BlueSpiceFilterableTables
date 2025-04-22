9b59b7730a490c88482cb68f81da4a941a37fb96
hslater <hamish.slater@hallowelt.com>
2025-04-22 16:00:30 +1200

    eslint
    ERM20742
    
    [REL1_43, REL1_43-5.0.x, master]
    
    Change-Id: I3115e45b60ea0b2e376b6035f6a7787748079f44
    (cherry picked from commit 3d36878da074ebfa2c4d7e7b5c763e385c46aa95)

--- resources/ui/grid/FilterableTable.js
+++ resources/ui/grid/FilterableTable.js
@@ -18,19 +18,19 @@ bs.filterableTables.ui.grid.FilterableTable = function ( cfg ) {
 
 	bs.filterableTables.ui.grid.FilterableTable.super.call( this, cfg );
 
-	let classes = this.getTableClassNames( cfg.$table );
-	this.$table.addClass( classes.join( ' ' ) );
+	const classes = this.getTableClassNames( cfg.$table );
+	this.$table.addClass( classes.join( ' ' ) ); // eslint-disable-line mediawiki/class-doc
 };
 
 OO.inheritClass( bs.filterableTables.ui.grid.FilterableTable, OOJSPlus.ui.data.GridWidget );
 
 bs.filterableTables.ui.grid.FilterableTable.prototype.makeGridCfg = function ( cfg ) {
-	let $table = cfg.$table || '';
+	const $table = cfg.$table || '';
 	if ( $table.find( 'caption' ).length > 0 ) {
-		let captionNodes = cfg.$table.find( 'caption' )[0].childNodes;
-		var caption = '';
-		for ( var i in captionNodes ) {
-			if ( captionNodes[ i ].nodeType != Node.TEXT_NODE ) {
+		const captionNodes = cfg.$table.find( 'caption' )[ 0 ].childNodes;
+		let caption = '';
+		for ( const i in captionNodes ) {
+			if ( captionNodes[ i ].nodeType !== Node.TEXT_NODE ) {
 				continue;
 			}
 			caption = captionNodes[ i ];
@@ -38,9 +38,9 @@ bs.filterableTables.ui.grid.FilterableTable.prototype.makeGridCfg = function ( c
 		cfg.caption = caption;
 	}
 	this.columnsHelper = {};
-	let columnCounter = 0;
-	if ( $table.find( 'thead tr').first().length < 1 ) {
-		let firstRow = $table.find( 'tbody tr' ).first();
+	const columnCounter = 0;
+	if ( $table.find( 'thead tr' ).first().length < 1 ) {
+		const firstRow = $table.find( 'tbody tr' ).first(); // eslint-disable-line no-jquery/variable-pattern
 		if ( firstRow.find( 'th' ).length > 0 ) {
 			this.processColumns( 'th', firstRow, columnCounter );
 			this.skipRows++;
@@ -49,7 +49,7 @@ bs.filterableTables.ui.grid.FilterableTable.prototype.makeGridCfg = function ( c
 			this.processColumns( 'th', firstRow, columnCounter );
 		}
 	} else {
-		let headerRow = $table.find( 'thead tr' ).first();
+		const headerRow = $table.find( 'thead tr' ).first(); // eslint-disable-line no-jquery/variable-pattern
 		this.processColumns( 'th', headerRow, columnCounter );
 	}
 	cfg.columns = this.columnsHelper;
@@ -57,76 +57,76 @@ bs.filterableTables.ui.grid.FilterableTable.prototype.makeGridCfg = function ( c
 	cfg.pageSize = 999;
 	if ( $table.hasClass( 'mw-collapsible' ) ) {
 		cfg.collapsible = true;
-		if( cfg.$table.hasClass( 'mw-collapsed' ) ) {
+		if ( cfg.$table.hasClass( 'mw-collapsed' ) ) {
 			cfg.collapsed = true;
 		}
 	}
-	if ( $table.hasClass( 'bs-exportable') ) {
+	if ( $table.hasClass( 'bs-exportable' ) ) {
 		cfg.exportable = true;
-		cfg.provideExportData = $table;
+		cfg.provideExportData = $table; // eslint-disable-line no-jquery/variable-pattern
 	}
 
 	return cfg;
 };
 
 bs.filterableTables.ui.grid.FilterableTable.prototype.getTableClassNames = function ( $table ) {
-	let tableClasses = $( $table )[0].className.split( ' ' );
+	const tableClasses = $( $table )[ 0 ].className.split( ' ' );
 
-	let classes = [];
-	for( let i in tableClasses ) {
-		let tableClass = tableClasses[i];
+	const classes = [];
+	for ( const i in tableClasses ) {
+		const tableClass = tableClasses[ i ];
 		if ( this.skipTableClasses.includes( tableClass ) ) {
 			continue;
 		}
 		classes.push( tableClass );
 	}
 	return classes;
-}
+};
 
 bs.filterableTables.ui.grid.FilterableTable.prototype.getData = function ( $table ) {
-	var me = this;
-	var currentData = [],
-		colspan = 0,
-		colspanVal = '',
-		rowspans = {};
-	$table.find( 'tbody tr' ).each(function() {
-		if( me.skipRows > 0 ) {
-			me.skipRows --;
+	const me = this;
+	const currentData = [];
+	let colspan = 0;
+	let colspanVal = '';
+	const rowspans = {};
+	$table.find( 'tbody tr' ).each( function () {
+		if ( me.skipRows > 0 ) {
+			me.skipRows--;
 			return;
 		}
-		var row = {};
-		var i = 0;
-		$( this ).find( 'th, td' ).each( function( index, value ) {
-			var attributes = me.getElAttributes( $(value) );
-			me.formatDate( $(value) );
+		const row = {};
+		let i = 0;
+		$( this ).find( 'th, td' ).each( ( index, value ) => {
+			const attributes = me.getElAttributes( $( value ) );
+			me.formatDate( $( value ) );
 
 			if ( colspan > 1 ) {
-				row[i] = me.getElText( colspanVal );
-				colspan --;
+				row[ i ] = me.getElText( colspanVal );
+				colspan--;
 				i++;
 			}
-			while ( rowspans[i] && rowspans[i].rowspan > 1 ) {
-				row[i] = me.getElText(
-					rowspans[i].rowspanVal
+			while ( rowspans[ i ] && rowspans[ i ].rowspan > 1 ) {
+				row[ i ] = me.getElText(
+					rowspans[ i ].rowspanVal
 				);
-				rowspans[i].rowspan --;
+				rowspans[ i ].rowspan--;
 				i++;
 			}
 
-			if ( attributes.colspan && attributes.colspan !== 0) {
+			if ( attributes.colspan && attributes.colspan !== 0 ) {
 				colspan = parseInt( attributes.colspan );
-				colspanVal = $(value);
+				colspanVal = $( value ); // eslint-disable-line no-jquery/variable-pattern
 			}
 			if ( attributes.rowspan && attributes.rowspan !== 0 ) {
-				rowspans[i] = {
+				rowspans[ i ] = {
 					rowspan: parseInt( attributes.rowspan ),
-					rowspanVal: $(value)
+					rowspanVal: $( value )
 				};
 			}
 
-			row[i] = me.getElText( $(value) );
+			row[ i ] = me.getElText( $( value ) );
 			i++;
-		}) ;
+		} );
 		currentData.push( row );
 	} );
 	return currentData;
@@ -141,9 +141,9 @@ bs.filterableTables.ui.grid.FilterableTable.prototype.processColumns = function 
 
 bs.filterableTables.ui.grid.FilterableTable.prototype.extractMappings = function ( $th, counter ) {
 	counter = counter || 0;
-	var attributes = this.getElAttributes( $th );
+	const attributes = this.getElAttributes( $th );
 
-	let columnConfig = {
+	const columnConfig = {
 		headerText: this.getElHeaderText( $th, counter + 1 ),
 		sortable: true,
 		type: attributes.type,
@@ -151,8 +151,8 @@ bs.filterableTables.ui.grid.FilterableTable.prototype.extractMappings = function
 			type: attributes.type
 		},
 		autoClosePopup: true
-	}
-	width = this.parseColumnWidthFromAttribute( attributes.style );
+	};
+	const width = this.parseColumnWidthFromAttribute( attributes.style );
 	if ( width ) {
 		columnConfig.width = width;
 	}
@@ -160,28 +160,28 @@ bs.filterableTables.ui.grid.FilterableTable.prototype.extractMappings = function
 };
 
 bs.filterableTables.ui.grid.FilterableTable.prototype.getElAttributes = function ( $el ) {
-	var dataTypes = [
+	const dataTypes = [
 		'text',
 		'integer',
 		'number',
 		'date'
 	];
-	var attr = {
-		'rowspan': false,
-		'colspan': false,
-		'type': 'text',
-		'align': 'left',
-		'style': ''
+	const attr = {
+		rowspan: false,
+		colspan: false,
+		type: 'text',
+		align: 'left',
+		style: ''
 	};
-	if ( $el[0].attributes ) {
-		$.each( $el[0].attributes, function() {
-			attr[this.name] = this.value;
+	if ( $el[ 0 ].attributes ) {
+		$.each( $el[ 0 ].attributes, function () { // eslint-disable-line no-jquery/no-each-util
+			attr[ this.name ] = this.value;
 		} );
 	}
-	for ( var i = 0; i < dataTypes.length; i++ ) {
-		if ( $el.hasClass( dataTypes[i] ) ) {
-			var type = dataTypes[i];
-			if( type === 'integer' ) { //backwards compatibility
+	for ( let i = 0; i < dataTypes.length; i++ ) {
+		if ( $el.hasClass( dataTypes[ i ] ) ) {
+			let type = dataTypes[ i ];
+			if ( type === 'integer' ) { // backwards compatibility
 				type = 'number';
 			}
 			attr.type = type;
@@ -190,31 +190,31 @@ bs.filterableTables.ui.grid.FilterableTable.prototype.getElAttributes = function
 	return attr;
 };
 
-bs.filterableTables.ui.grid.FilterableTable.prototype.parseColumnWidthFromAttribute = function( style ) {
-	if ( style || style.includes('width') ) {
-		result = style.replace(" ", "").match( 'width:([0-9]*)(\%|px|em)' );
+bs.filterableTables.ui.grid.FilterableTable.prototype.parseColumnWidthFromAttribute = function ( style ) {
+	if ( style || style.includes( 'width' ) ) {
+		const result = style.replace( ' ', '' ).match( 'width:([0-9]*)(%|px|em)' );
 		if ( result !== null ) {
-			if ( result[2] === "px" ) {
-				return parseInt( result[1] );
+			if ( result[ 2 ] === 'px' ) {
+				return parseInt( result[ 1 ] );
 			}
-			if ( result[2] === "em" ) {
-				return parseInt( result[1] ) * 12.8;
+			if ( result[ 2 ] === 'em' ) {
+				return parseInt( result[ 1 ] ) * 12.8;
 			}
-			if ( result[2] === "%" ) {
-				return result[1] + result[2];
+			if ( result[ 2 ] === '%' ) {
+				return result[ 1 ] + result[ 2 ];
 			}
 		}
 	}
 	return null;
 };
 
-bs.filterableTables.ui.grid.FilterableTable.prototype.getElHeaderText = function( $el, counter ) {
-	if ( $el.is('th') ) {
+bs.filterableTables.ui.grid.FilterableTable.prototype.getElHeaderText = function ( $el, counter ) {
+	if ( $el.is( 'th' ) ) {
 		if ( $( $el ).children().length === 0 ) {
 			return this.getElText( $el ).replace( /\n/g, '' );
 		}
-		let headerNodes = $( $el ).contents();
-		for ( var i = 0; i < headerNodes.length; i++ ) {
+		const headerNodes = $( $el ).contents(); // eslint-disable-line no-jquery/variable-pattern
+		for ( let i = 0; i < headerNodes.length; i++ ) {
 			if ( headerNodes[ i ].nodeType === Node.ELEMENT_NODE && headerNodes[ i ].tagName === 'BUTTON' ) {
 				continue;
 			}
@@ -224,28 +224,28 @@ bs.filterableTables.ui.grid.FilterableTable.prototype.getElHeaderText = function
 	return counter || '-';
 };
 
-bs.filterableTables.ui.grid.FilterableTable.prototype.getElText = function( $el ) {
-	var text = $el.text();
+bs.filterableTables.ui.grid.FilterableTable.prototype.getElText = function ( $el ) {
+	let text = $el.text();
 	text = text.replace( /(<([^>]+)>)/ig, '' );
-	text = $.trim( text );
+	text = $.trim( text ); // eslint-disable-line no-jquery/no-trim
 	return text;
 };
 
-bs.filterableTables.ui.grid.FilterableTable.prototype.formatDate = function( $el ) {
+bs.filterableTables.ui.grid.FilterableTable.prototype.formatDate = function ( $el ) {
 	// small HACK to make date in format dd.mm.yyyy|dd-mm-yyyy sortable
-	if (date = $el[0].textContent.trim()
-		.match(/^([0-9]{2})(.|-)([0-9]{2})(.|-)([0-9]{4})$/)
-	) {
-		$el[0].textContent = ``;
+	const date = $el[ 0 ].textContent.trim()
+		.match( /^([0-9]{2})([.-])([0-9]{2})\2([0-9]{4})$/ );
+	if ( date ) {
+		$el[ 0 ].textContent = '';
 
-		var invisibleDate = document.createElement('p');
-		invisibleDate.textContent = `${date[5]}.${date[3]}.${date[1]}`;
-		invisibleDate.style.display='none';
+		const invisibleDate = document.createElement( 'p' );
+		invisibleDate.textContent = `${ date[ 5 ] }.${ date[ 3 ] }.${ date[ 1 ] }`;
+		invisibleDate.style.display = 'none';
 
-		var visibleDate = document.createElement('div');
-		visibleDate.textContent = `${date[1]}.${date[3]}.${date[5]}`;
+		const visibleDate = document.createElement( 'div' );
+		visibleDate.textContent = `${ date[ 1 ] }.${ date[ 3 ] }.${ date[ 5 ] }`;
 
-		$el[0].appendChild( invisibleDate );
-		$el[0].appendChild( visibleDate );
+		$el[ 0 ].appendChild( invisibleDate );
+		$el[ 0 ].appendChild( visibleDate );
 	}
 };
