76183584fc9089c642640ff88c50e91f42e3032a
Hamish <hamish.slater@hallowelt.com>
2026-07-09 22:49:59 +0700

    phpcs
    Change-Id: If1fb63f47573d592db2c809fe735f2d690ecfa32

--- src/Hook/BeforePageDisplay/AddResources.php
+++ src/Hook/BeforePageDisplay/AddResources.php
@@ -4,6 +4,9 @@ namespace BlueSpice\FilterableTables\Hook\BeforePageDisplay;
 
 class AddResources extends \BlueSpice\Hook\BeforePageDisplay {
 
+	/**
+	 * @return bool
+	 */
 	protected function doProcess() {
 		$this->out->addModules( 'ext.bluespice.filterabletables' );
 		return true;
